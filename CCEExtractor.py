326fddfa01347851aa7cb95119eabc2c3a1689ae
DTechnology <djakajaya89@gmail.com>
2018-11-05 10:43:07 +0700

    First Comit

---
+++ CCEExtractor.py
@@ -0,0 +1,5 @@
+for i in range(0, 10):
+    print("GCI is great")
+name = input("Hey, whats your name ?")
+print("Hello",name,", please to meet you!")
+print("Hello ",name,", please to meet you! Did you know that your name backwards is",''.join(reversed(name)),"?")
